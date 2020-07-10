ecb708a7c770e29bf55b802af3f1c2bc694288c7
PhilCAEUser <rbyETE4Ceu2iA4Z>
2020-07-10 13:03:32 +0000

    Microservice Init

---
+++ db/405_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  caeschema
+-- Automatically generated sql script for the service 405, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table tab.
+--
+CREATE TABLE caeschema.tab (
+  col1 INTEGER ,
+CONSTRAINT col1_PK PRIMARY KEY (col1)
+ 
+);
+
+
+
